750c9b9afe99e5942758a3860783b5da0f28d8ef
rivaldiscorp <rivdew@gmail.com>
2021-02-22 00:51:20 +0700

    Fix bug in LocaleDataSource add method list and update

--- Sources/Core/Data/LocaleDataSource.swift
+++ Sources/Core/Data/LocaleDataSource.swift
@@ -12,7 +12,9 @@ public protocol LocaleDataSource {
     associatedtype Request
     associatedtype Response
     
+    func list(request: Request?) -> Observable<[Response]>
     func get(request: Request?) -> Observable<[Response]>
     func add(from games: [Response]) -> Observable<Bool>
+    func update(id: Int, entity: Response) -> Observable<Bool>
 
 }
