ba3d632b10cd8168f342a6ce7575aca68df5a09d
rivaldiscorp <rivdew@gmail.com>
2021-03-02 23:17:16 +0700

    Menambahkan GetDetailPresenter

---
+++ Sources/Core/Presentation/GetDetailPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rivaldi Aliando on 02/03/21.
+//
+
+import SwiftUI
+import RxSwift
+
+public class GetListPresenter<Request, Response, Interactor: UseCase>: ObservableObject where Interactor.Request == Request, Interactor.Response == Response {
+    
+    private var disposeBag = DisposeBag()
+    
+    private let _useCase: Interactor
+    
+    @Published public var detailData: Response?
+    @Published public var errorMessage: String = ""
+    @Published public var isLoading: Bool = false
+    @Published public var isError: Bool = false
+    
+    public init(useCase: Interactor) {
+        _useCase = useCase
+    }
+    
+    public func getDetail(request: Request?) {
+        isLoading = true
+        _useCase.execute(request: request)
+            .observeOn(MainScheduler.instance)
+            .subscribe { data in
+                self.detailData = data
+            } onError: { error in
+                self.errorMessage = error.localizedDescription
+                self.isError = true
+                self.isLoading = false
+            } onCompleted: {
+                self.isLoading = false
+            }.disposed(by: disposeBag)
+    }
+}
